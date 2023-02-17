e774931b5a39469d92bd9c7ab305c6fb7067a8cc
Nath <nideta231@gmail.com>
2023-02-17 11:33:50 +0700

    Test: Adding multiple GET request dummy data

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,53 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  getUsers() {
+    return [{ username: 'Nath', email: 'nath231@nath.com' }];
+  }
+
+  @Get('posts')
+  getUsersPosts() {
+    return [
+      {
+        username: 'Nath',
+        email: 'nath231@nath.com',
+        posts: [
+          {
+            id: 1,
+            title: 'Post 1',
+          },
+          {
+            id: 2,
+            title: 'Post 2',
+          },
+        ],
+      },
+    ];
+  }
+
+  @Get('posts/comments')
+  getUsersPostsComments(){
+    return [
+      {
+        posts: [
+          {
+            id: 1,
+            title: 'Post 1',
+            comments: [
+              {
+                id: 1,
+                postId: 1,
+                userId: 1,
+                username: 'Nath',
+                comment: 'Hey, i am Nath!!',
+                likes: 5
+              },
+            ],
+          },
+        ],
+      },
+    ];
+  }
+}
